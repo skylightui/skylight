e8beae46460999c8ff68c9aaa1b95882b621211f
Skylight <stuart@stuartlewis.com>
2011-06-03 19:18:03 +1200

    Update sample configuration file

--- application/config/skylight-sample.php
+++ application/config/skylight-sample.php
@@ -1,30 +1,30 @@
 <?php
 
     // The URL of the parent solr server
-    $config['skylight_solrbase'] = 'http://titanio.lbr.auckland.ac.nz:8080/solr/search/';
+    $config['skylight_solrbase'] = 'http://your.repository.org:8080/solr/search/';
 
     // The OAI-PMH base for the parent server
-    $config['skylight_oaipmhbase'] = 'http://magnesio.lbr.auckland.ac.nz:8080/oai/request?';
+    $config['skylight_oaipmhbase'] = 'http://your.repository.org:8080/oai/request?';
 
     // The OAI-PMH identifier to replace in OAI-PMH responses
-    $config['skylight_oaipmhid'] = 'oai:magnesio.lbr.auckland.ac.nz:123456789/';
+    $config['skylight_oaipmhid'] = 'your.repository.org:123456789/';
 
     // The link in OAI-PMH responses to replace with the skylight record URL
     $config['skylight_oaipmhlink'] = 'http://hdl.handle.net/123456789/';
 
     // The URL base for where digital objects can be proxied from
-    $config['skylight_objectproxy_url'] = 'http://magnesio.lbr.auckland.ac.nz:8080/bitstream/123456789/';
+    $config['skylight_objectproxy_url'] = 'http://your.repository.org:8080/bitstream/123456789/';
 
     // The URL base for where digital objects can be proxied from
-    $config['skylight_objectproxy_url'] = 'http://magnesio.lbr.auckland.ac.nz:8080/bitstream/123456789/';
+    $config['skylight_objectproxy_url'] = 'http://your.repository.org:8080/bitstream/123456789/';
 
     // Set to the number of minutes to cache pages for. Set to false for no caching.
     // This can be overridden in site-specific configuration files.
     $config['skylight_cache'] = false;
 
     // Keys required for the recapthca system
-    $config['skylight_recaptcha_key_public'] = '6LfEH8QSAAAAAO5IBW9jdsenw5CCjaFF_AzeyQCc';
-    $config['skylight_recaptcha_key_private'] = '6LfEH8QSAAAAANcNg6_0inIHSFf_IkgYuZYHJEfe';
+    $config['skylight_recaptcha_key_public'] = 'ENTER-HERE';
+    $config['skylight_recaptcha_key_private'] = 'ENTER-HERE';
 
     // Digital object management
     $config['skylight_bitstream_field'] = 'superindex.bitstreams.original.en';
