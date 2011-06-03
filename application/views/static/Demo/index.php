f6b2bb482bc409f6cc375daa889a645f1c0a429c
Skylight <stuart@stuartlewis.com>
2011-06-03 19:13:31 +1200

    New demo static home page example file

---
+++ application/views/static/Demo/index.php
@@ -0,0 +1,6 @@
+<div class="record">
+    <p>
+        This is a default 'welcome' page that can be overridden by editing
+        <code>[skylight]/application/views/static/Demo/index.php</code>
+    </p>
+</div>
