b772a084aa65b27930898447284cb959e8f50e44
Skylight <stuart@stuartlewis.com>
2011-07-25 16:09:43 +1200

    [SI-53] External theme path for Skylight - allow static content to be help outside, loaded from the foreign.php view

--- application/views/foreign.php
+++ application/views/foreign.php
@@ -1,5 +1,8 @@
 <?php
     // This is empty view used for loading foreign content
     // (held outside of the normal CodeIgniter views directory)
+    //
+    // This must be performed from within a view so that the
+    // buffering is applied in order
     include($load);
 ?>
