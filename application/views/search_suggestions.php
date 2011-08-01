1a12f40ed6ae7737071bc69af649d81acb7ed34c
Kim Shepherd <kim.shepherd@gmail.com>
2011-08-02 10:36:11 +1200

    * Search term highlighting * Spellcheck/auto-suggest in search results

---
+++ application/views/search_suggestions.php
@@ -0,0 +1,11 @@
+<?php if($suggestion != NULL && $suggestion != "") {
+   ?>
+
+        <div class="suggestion">You searched for "<?php echo urldecode($query) ?>".
+    Did you mean <a href="./search/<?php echo $suggestion ?>">
+                <?php echo $suggestion ?>
+            </a></div>
+
+
+   <?php
+}?>
