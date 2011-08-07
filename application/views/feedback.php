629999055055413353a37e68008ffa3ff56cbeb7
Skylight <stuart@stuartlewis.com>
2011-08-08 11:47:06 +1200

    Add <label> elements to feedback form.

--- application/views/feedback.php
+++ application/views/feedback.php
@@ -1,5 +1,8 @@
 <div class="feedback_form">
+    <p>We welcome your feedback.  Please leave your suggestions or questions below.</p>
+
     <?php echo validation_errors(); ?>
+
     <script type="text/javascript">
      var RecaptchaOptions = {
         theme : 'clean'
@@ -7,12 +10,18 @@
      </script>
 
     <?php echo form_open('feedback'); ?>
-        Name: <input type="text" name="name" value="<?php echo set_value('name'); ?>" /><br />
-        Email: <input type="text" name="email" value="<?php echo set_value('email'); ?>" /><br />
-        Feedback: <textarea type="text" name="feedback" rows="15" cols="80" /><?php echo set_value('feedback'); ?></textarea><br />
-        Please enter the following verification words into the box:<?php
+        <label for="name">Name:</label> <input type="text" id="name" name="name" size="50" value="<?php echo set_value('name'); ?>" /><br />
+        <label for="email">Email:</label> <input type="text" id="email" name="email" size="50" value="<?php echo set_value('email'); ?>" /><br />
+        <label for="feedback">Feedback:</label> <textarea type="text" id="feedback" name="feedback" rows="15" cols="80" /><?php echo set_value('feedback'); ?></textarea><br />
+
+        <p>&nbsp;</p>
+
+        <label>Please enter the following verification words into the box:</label> <?php
             echo recaptcha_get_html($recaptcha_key_public);
         ?>
-        <input type="submit" />
+
+        <p>&nbsp;</p>
+
+        <input type="submit" value="Send feedback..." />
     </form>
 </div>
