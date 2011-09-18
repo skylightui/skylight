c5f4ea62bbb3a350f1f41d7e0251000f72cc41aa
Skylight <stuart@stuartlewis.com>
2011-09-19 11:29:21 +1200

    Adding very rudimentary json support to search result pages

---
+++ application/views/formats/json/search_results.php
@@ -0,0 +1,22 @@
+<?php
+
+    $records = array();
+
+    foreach ($docs as $doc) {
+
+        // JSON serialize a record
+        $record = array();
+
+        foreach($doc as $key => $element) {
+
+                $record[$key] = $element;
+
+        }
+
+        $records[] = array('record' => $record);
+
+    }
+
+    echo json_encode(array('records' => $records));
+
+?>
