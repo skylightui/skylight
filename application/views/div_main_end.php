9c14cd15e6b05ff2c48b672d7dd3980b0ab9edec
Skylight <stuart@stuartlewis.com>
2011-11-01 13:40:47 +1300

    Fixing footer entries

--- application/views/div_main_end.php
+++ application/views/div_main_end.php
@@ -2,9 +2,8 @@
         <footer>
             <a href="./"><?php echo $site_title; ?></a>
             <a href="./about">About this site</a>
-            <a href="./feedback" class="last">Feedback</a><br />
-            <a href="http://skylightui.org/">Powered by Skylight</a>
-            <img src="./assets/images/skylight.jpg" />
+            <a href="./feedback">Feedback</a>
+            <a href="http://skylightui.org/"class="last">Powered by Skylight</a>
         </footer>
     
 </div>
