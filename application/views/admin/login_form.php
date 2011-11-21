7920ff3b5d5e81ac6a2de8e69ff6c96f47b12489
Skylight <stuart@stuartlewis.com>
2011-11-21 14:30:17 +1300

    Remove old CSS references

--- application/views/admin/login_form.php
+++ application/views/admin/login_form.php
@@ -5,25 +5,17 @@
 
         <?php echo form_open('adminlogin'); ?>
 
-        <div class="section">
-
-            <div class="formtext">
-                <label for="username">Username:</label>
-                <input type="text" id="username" name="username" size="30" value="<?php echo set_value('username'); ?>" />
-            </div>
-
-            <div class="formtext">
-                <label for="password">Password:</label>
-                <input type="password" id="password" name="password" size="30" />
-            </div>
-
+        <div class="formtext">
+            <label for="username">Username:</label>
+            <input type="text" id="username" name="username" size="30" value="<?php echo set_value('username'); ?>" />
         </div>
 
-        <div class="section">
-
-            <input type="Submit" name="submit" id="submit" value="Next &gt;" />
-
+        <div class="formtext">
+            <label for="password">Password:</label>
+            <input type="password" id="password" name="password" size="30" />
         </div>
+
+        <input type="Submit" name="submit" id="submit" value="Next &gt;" />
 
         <?php echo form_close(); ?>
 
