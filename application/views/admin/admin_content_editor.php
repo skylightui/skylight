adc127b4f57a335575e6be0ad4792411fdfb2309
kshepherd <kim.shepherd@gmail.com>
2012-09-06 08:39:30 +1200

    Kshitij's fix for tinyMCE

--- application/views/admin/admin_content_editor.php
+++ application/views/admin/admin_content_editor.php
@@ -3,9 +3,34 @@
     <form method="post" action="./admin/savecontent">
         <input type="hidden" name="content" value="<?php echo $content; ?>" />
         <p>
-                <textarea name="html" cols="90" rows="25"><?php echo $html; ?></textarea>
-                <input type="submit" value="Save" />
+            <textarea name="html" cols="90" rows="25"><?php echo $html; ?></textarea>
+            <input type="submit" value="Save" />
         </p>
     </form>
 
 </div>
+
+<script type="text/javascript" src="./assets/tiny_mce/tiny_mce.js"></script>
+<script type="text/javascript">
+tinyMCE.init({
+        // General options
+        mode : "textareas",
+        theme : "advanced",
+        plugins : "autolink,lists,spellchecker,pagebreak,style,layer,table,save,advhr,advimage,advlink,emotions,iespell,inlinepopups,insertdatetime,preview,media,searchreplace,print,contextmenu,paste,directionality,fullscreen,noneditable,visualchars,nonbreaking,xhtmlxtras,template",
+        
+        theme_advanced_buttons1 : "save,newdocument,|,bold,italic,underline,strikethrough,|,justifyleft,justifycenter,justifyright,justifyfull,|,styleselect,formatselect,fontselect,fontsizeselect",
+        theme_advanced_buttons2 : "cut,copy,paste,pastetext,pasteword,|,search,replace,|,bullist,numlist,|,outdent,indent,blockquote,|,undo,redo,|,link,unlink,anchor,image,cleanup,help,code,|,insertdate,inserttime,preview,|,forecolor,backcolor",
+        theme_advanced_buttons3 : "tablecontrols,|,hr,removeformat,visualaid,|,sub,sup,|,charmap,emotions,iespell,media,advhr,|,print,|,ltr,rtl,|,fullscreen",
+        theme_advanced_buttons4 : "insertlayer,moveforward,movebackward,absolute,|,styleprops,spellchecker,|,cite,abbr,acronym,del,ins,attribs,|,visualchars,nonbreaking,template,blockquote,pagebreak,|,insertfile,insertimage",
+        theme_advanced_toolbar_location : "top",
+        theme_advanced_toolbar_align : "left",
+        theme_advanced_statusbar_location : "bottom",
+        theme_advanced_resizing : true,
+        
+        // Skin options
+        skin : "o2k7",
+        skin_variant : "silver"
+
+        
+});
+</script>
