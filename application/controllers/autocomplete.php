2b187e35106267569827a73b7f0b2cc82a471580
Robin Taylor <robin.taylor@ed.ac.uk>
2014-02-21 14:37:00 +0000

    LIB001-519 Update the autocomplete controller

--- application/controllers/autocomplete.php
+++ application/controllers/autocomplete.php
@@ -11,17 +11,21 @@ class Autocomplete extends skylight {
 
 	public function index() {
 
+        log_message('debug', "search autocomplete initialized");
+
         $term = $this->input->get('term');
         $field = $this->input->get('field');
+        $term_lower = strtolower($term);
 
-        $solr_xml = file_get_contents($this->config->item('skylight_solrbase') . 'terms?terms=true&terms.fl='.$field.'&terms.prefix='.$term.'&terms.lower.incl=false&terms.regex.flag=case_insensitive&indent=true&wt=json');
+        //$solr_xml = file_get_contents($this->config->item('skylight_solrbase') . 'terms?terms=true&terms.fl='.$field.'&terms.prefix='.$term.'&terms.lower.incl=false&terms.regex.flag=case_insensitive&indent=true&wt=json');
+        $solr_xml = file_get_contents($this->config->item('skylight_solrbase') . 'terms?terms=true&terms.fl=title_ac&terms.prefix='.$term_lower.'&terms.lower.incl=false&terms.regex.flag=case_insensitive&indent=true&wt=json');
 
         $ac_json = json_decode($solr_xml);
 
         echo '[';
-        for($i = 0; $i < sizeof($ac_json->terms[1]); $i += 2) {
-            echo '"'.$ac_json->terms[1][$i].'"';
-            if($i+2 < sizeof($ac_json->terms[1])) {
+        for($i = 0; $i < sizeof($ac_json->terms->{'title_ac'}); $i += 2) {
+            echo '"'.$ac_json->terms->{'title_ac'}[$i].'"';
+            if($i+2 < sizeof($ac_json->terms->{'title_ac'})) {
                 echo ', ';
             }
         }
