e8ea9a7bfc71fb297aa23201e1cd9825ef30fea7
Robin Taylor <robin.taylor@ed.ac.uk>
2014-03-14 14:35:04 +0000

    LIB001-503 Correctly reference Jquery libs

--- application/views/footer.php
+++ application/views/footer.php
@@ -2,9 +2,10 @@
         </div> <!-- close main -->
         </div> <!-- close container -->
 
-    <script src="./assets/jquery/jquery-1.6.4.min.js"></script>
-    <script src="./assets/plugins/plugins.js"></script>
-    <script src="./assets/script/script.js"></script>
+        <script src="<?php echo base_url()?>assets/jquery-1.11.0/jquery-1.11.0.min.js"></script>
+        <script src="<?php echo base_url()?>assets/jquery-ui-1.10.4/ui/minified/jquery-ui.min.js"></script>
+        <script src="<?php echo base_url()?>assets/plugins/plugins.js"></script>
+        <script src="<?php echo base_url()?>assets/script/script.js"></script>
 
     </body>
 </html>
