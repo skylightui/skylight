37b768c6c5641bdfab145e84da2da204f483614e
Scott Renton <scott.renton@ed.ac.uk>
2014-03-25 10:15:53 +0000

    LIB001-468: OAIPMH changes for SPIRIT and MIMO

--- application/controllers/oaipmh/listrecords.php
+++ application/controllers/oaipmh/listrecords.php
@@ -2,18 +2,45 @@
 
     $url = $oaipmhbase . 'verb=ListRecords';
 
+
+    if (!(isset($_GET['metadataPrefix'])))
+    {
+        $metadataPrefix = '&metadataPrefix=oai_dc';
+
+    }
+    else
+    {
+        $metadataPrefix = '&metadataPrefix='.$_GET['metadataPrefix'];
+    }
+
+    if (!(isset($_GET['set'])))
+    {
+        $set = '&set=' . $oaipmhcollection;
+
+    }
+    else
+    {
+        $set = '&set='.$_GET['set'];
+    }
+
+
     if (!empty($_GET['resumptionToken'])) {
         $url .= '&resumptionToken=' . $_GET['resumptionToken'];
     } else {
-        $url .= '&set=' . $oaipmhcollection . '&metadataPrefix=oai_dc';
+        $url .= $metadataPrefix.$set;
     }
 
+
+
     $response = file_get_contents($url);
+
+    //print_r($response);
 
     $response = str_replace('<?xml version="1.0" encoding="UTF-8" ?>', '', $response);
     $response = str_replace(substr($oaipmhbase, 0, strlen($oaipmhbase) - 1), htmlentities($base), $response);
     $response = str_replace(' set="' . $oaipmhcollection . '">', '>', $response);
-    $response = str_replace('<identifier>' . $oaipmhid, '<identifier>oai:skylight/' . $id . '/', $response);
+    //$response = str_replace('<identifier>' . $oaipmhid, '<identifier>oai:skylight/' . $id . '/', $response);
+    $response = str_replace('<identifier>' . $oaipmhid, '<identifier>oai:skylight/', $response);
     $response = str_replace('<dc:identifier>' . $oaipmhlink, '<dc:identifier>' . $record_url, $response);
     $response = preg_replace("#<dc:identifier>(?:(?!".$record_url.").)*</dc:identifier>#",'', $response);
     $response = str_replace('<setSpec>' . $oaipmhcollection . '</setSpec>', '', $response);
