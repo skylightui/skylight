b133597e3ce29420e2d97e188c8d25cab25271c1
Ianthe Sutherland <ianthe88@gmail.com>
2014-03-28 11:46:52 +0000

    LIB001-441: Addition of thumbnail styling, fancybox and bootstrap folder

--- application/views/footer.php
+++ application/views/footer.php
@@ -7,5 +7,17 @@
         <script src="<?php echo base_url()?>assets/plugins/plugins.js"></script>
         <script src="<?php echo base_url()?>assets/script/script.js"></script>
 
+
+        <!-- Add mousewheel plugin (this is optional) -->
+        <script type="text/javascript" src="assets/fancybox/lib/jquery.mousewheel-3.0.6.pack.js"></script>
+        <!-- Add fancyBox -->
+        <script type="text/javascript" src="assets/fancybox/source/jquery.fancybox.pack.js?v=2.1.4"></script>
+        <!-- Optionally add helpers - button, thumbnail and/or media -->
+
+        <script type="text/javascript" src="assets/fancybox/source/helpers/jquery.fancybox-buttons.js?v=1.0.5"></script>
+        <script type="text/javascript" src="assets/fancybox/source/helpers/jquery.fancybox-media.js?v=1.0.5"></script>
+
+        <script type="text/javascript" src="assets/fancybox/source/helpers/jquery.fancybox-thumbs.js?v=1.0.7"></script>
+
     </body>
 </html>
