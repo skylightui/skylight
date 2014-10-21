8dc25fbabf2c968cfb1d069898dc2f3d37595a37
Ianthe Sutherland <ianthe88@gmail.com>
2014-10-21 13:18:48 +0100

    LIB001-914: Improving sitemaps - PDFs and external URLS

--- application/controllers/sitemap.php
+++ application/controllers/sitemap.php
@@ -10,20 +10,38 @@ class Sitemap extends skylight {
     }
 
     function _remap($query, $params = array()) {
-        // add in here which collections to include in sitemap for indexing
-        // currently
-        // 1: CLDS
-        // 3: Art
-        // 11: MIMEd
-        // 15: Calendars
-        $data = $this->solr_client->getSiteMapURLs("location.coll%3A%281+3+11+15%29");
-        $data["prefixes"] = array("1" => "", "3" => "art/", "11" => "mimed/", "15" => "calendars/");
+
+    $container = $this->config->item('skylight_container_field');
+    $container_id = $this->config->item('skylight_container_id');
+
+        if($this->config->item('skylight_sitemap_type') == "external") {
+
+            $data = $this->solr_client->getSiteMapURLs($container."%3A%28".$container_id."%29", $container);
+            $data["prefixes"] = array();
+        }
+
+        // internal sitemap or not set, so default to internal
+        else {
+
+            // add in here which collections to include in sitemap for indexing
+            // currently
+            // 1: CLDS
+            // 3: Art
+            // 11: MIMEd
+            // 15: Calendars
+            $data = $this->solr_client->getSiteMapURLs("location.coll%3A%281+OR+3+OR+11+OR+15%29", $container);
+            $data["prefixes"] = array("1" => "", "3" => "art/", "11" => "mimed/", "15" => "calendars/");
+
+        }
 
         // Set the headers
         header('Content-Type: application/xml; charset=utf-8');
         echo '<?xml version="1.0" encoding="UTF-8" ?>' . "\n";
 
+        //print_r($data);
+
         $this->view("sitemap", $data);
+
     }
 
 }
--- application/views/sitemap.php
+++ application/views/sitemap.php
@@ -1,4 +1,5 @@
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9" xmlns:image="http://www.google.com/schemas/sitemap-image/1.1">
+<?php if($this->config->item('skylight_sitemap_type') == "internal") { ?>
     <url>
         <loc><?php echo base_url();?></loc>
         <priority>1.0</priority>
@@ -38,6 +39,18 @@
         <priority>0.8</priority>
     </url>
 
+<?php } else { ?>
+    <url>
+        <loc><?php echo base_url();?></loc>
+        <priority>1.0</priority>
+    </url>
+    <url>
+        <loc><?php echo base_url() . "about";?></loc>
+        <priority>0.8</priority>
+    </url>
+
+
+<?php } ?>
 
     <!-- Record URLs -->
     <?php foreach($docs as $doc) { ?>
@@ -57,16 +70,28 @@
             if(array_key_exists("imageURL", $doc)) {
                 foreach($doc["imageURL"] as $imageURL) { ?>
 
-                <image:image>
-                    <image:loc><?php echo base_url().$prefix.$imageURL; ?></image:loc>
-                </image:image>
+                    <image:image>
+                        <image:loc><?php echo base_url().$prefix.$imageURL; ?></image:loc>
+                    </image:image>
 
-            <?php
+                <?php
                 }
             }
             ?>
             <priority>0.5</priority>
         </url>
+        <?php //check if there's a PDF
+        if(array_key_exists("pdfURL", $doc)) {
+            foreach($doc["pdfURL"] as $pdfURL) { ?>
+                <url>
+                    <loc><?php echo base_url().$prefix.str_replace("&", "%26", $pdfURL)?></loc>
+                    <priority>0.5</priority>
+                </url>
+            <?php
+            }
+        }
+        ?>
+
     <?php } ?>
 
 </urlset>
